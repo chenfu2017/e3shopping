407d28e9f581ec2e1bf22bebd9230d668e3a94e4
chenfu <211147056@qq.com>
2018-07-18 23:10:10 +0800

    三大框架搭建完成

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,27 @@
+package cn.e3mall.service.impl;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
+import cn.e3mall.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private TbItemMapper itemMapper;
+    @Override
+    public TbItem getTbItemById(long itemId) {
+        TbItemExample tbItemExample = new TbItemExample();
+        TbItemExample.Criteria criteria = tbItemExample.createCriteria();
+        criteria.andIdEqualTo(itemId);
+        List<TbItem> list = itemMapper.selectByExample(tbItemExample);
+        if(list!=null&&list.size()>0){
+            return list.get(0);
+        }
+        return null;
+    }
+}
