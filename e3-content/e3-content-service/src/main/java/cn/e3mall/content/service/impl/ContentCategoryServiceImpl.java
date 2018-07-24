942e2fff696c893f8d1ba8d4919393715d73048d
chenfu <211147056@qq.com>
2018-07-24 11:05:23 +0800

    内容分类列表添加

--- e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentCategoryServiceImpl.java
+++ e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentCategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.e3mall.content.service.impl;
 
 import cn.e3mall.common.pojo.EasyUITreeNode;
+import cn.e3mall.common.utils.E3Result;
 import cn.e3mall.content.service.ContentCategoryService;
 import cn.e3mall.mapper.TbContentCategoryMapper;
 import cn.e3mall.pojo.TbContentCategory;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 @Service
 public class ContentCategoryServiceImpl implements ContentCategoryService {
@@ -30,5 +32,27 @@ public class ContentCategoryServiceImpl implements ContentCategoryService {
             list.add(treeNode);
         }
         return list;
+    }
+
+    @Override
+    public E3Result addContentCategory(long parentId, String name) {
+        TbContentCategory contentCategory = new TbContentCategory();
+        contentCategory.setName(name);
+        contentCategory.setParentId(parentId);
+        //1 正常 2 删除
+        contentCategory.setStatus(1);
+        //排序方法默认为1
+        contentCategory.setSortOrder(1);
+        contentCategory.setIsParent(false);
+        contentCategory.setCreated(new Date());
+        contentCategory.setUpdated(new Date());
+        contentCategoryMapper.insert(contentCategory);
+        TbContentCategory parent = contentCategoryMapper.selectByPrimaryKey(parentId);
+        if (!parent.getIsParent()) {
+            parent.setIsParent(true);
+        }
+        contentCategoryMapper.updateByPrimaryKey(parent);
+
+        return E3Result.ok(contentCategory);
     }
 }
--- e3-manager-web/src/main/java/cn/e3mall/controller/ContentCatController.java
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ContentCatController.java
@@ -1,13 +1,11 @@
 package cn.e3mall.controller;
 
 import cn.e3mall.common.pojo.EasyUITreeNode;
+import cn.e3mall.common.utils.E3Result;
 import cn.e3mall.content.service.ContentCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,11 @@ public class ContentCatController {
             @RequestParam(name = "id",defaultValue = "0") Long parentId) {
         List<EasyUITreeNode> list = contentCategoryService.getContentCatList(parentId);
         return list;
+    }
+    @RequestMapping(value = "/content/category/create",method = RequestMethod.POST)
+    @ResponseBody
+    public E3Result addContentCategory(Long parentId,String name){
+        E3Result e3Result = contentCategoryService.addContentCategory(parentId, name);
+        return e3Result;
     }
 }
