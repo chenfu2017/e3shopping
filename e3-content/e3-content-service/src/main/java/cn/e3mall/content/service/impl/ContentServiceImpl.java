0a5fd3fa88ce26c17d98401d4406b1969007ee4f
chenfu <211147056@qq.com>
2018-07-24 23:14:26 +0800

    jedis

--- e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
+++ e3-content/e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
@@ -4,10 +4,13 @@ import cn.e3mall.common.utils.E3Result;
 import cn.e3mall.content.service.ContentService;
 import cn.e3mall.mapper.TbContentMapper;
 import cn.e3mall.pojo.TbContent;
+import cn.e3mall.pojo.TbContentExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+
 @Service
 public class ContentServiceImpl implements ContentService {
 
@@ -19,5 +22,14 @@ public class ContentServiceImpl implements ContentService {
         content.setUpdated(new Date());
         contentMapper.insertSelective(content);
         return E3Result.ok();
+    }
+
+    @Override
+    public List<TbContent> getContentListByCid(long cid) {
+        TbContentExample example = new TbContentExample();
+        TbContentExample.Criteria criteria = example.createCriteria();
+        criteria.andCategoryIdEqualTo(cid);
+        List<TbContent> contentList = contentMapper.selectByExampleWithBLOBs(example);
+        return contentList;
     }
 }
