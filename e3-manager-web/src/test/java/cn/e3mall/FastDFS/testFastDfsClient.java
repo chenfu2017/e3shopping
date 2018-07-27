9c26996ea5ad73a686909fbf37963e3b8b8be90e
chenfu <211147056@qq.com>
2018-07-27 10:38:22 +0800

    图片上传功能

---
+++ e3-manager-web/src/test/java/cn/e3mall/FastDFS/testFastDfsClient.java
@@ -0,0 +1,16 @@
+package cn.e3mall.FastDFS;
+
+public class testFastDfsClient {
+/*
+
+    @Test
+    public void fun() throws Exception{
+        FastDFSClient fastDFSClient = new FastDFSClient("C:/Users/chenfu/IdeaProjects/e3shopping/e3-manager-web/src/main/resources/conf/client.conf");
+        String file = fastDFSClient.uploadFile("C:/Users/chenfu/Pictures/Camera Roll/1 (1).jpg");
+
+        System.out.println(file);
+    }
+*/
+
+
+}
--- e3-manager/e3-manager-service/src/test/java/cn/e3mall/activemq/ActivemqTest.java
+++ e3-manager/e3-manager-service/src/test/java/cn/e3mall/activemq/ActivemqTest.java
@@ -1,17 +1,7 @@
 package cn.e3mall.activemq;
 
-import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
-
-import javax.jms.Destination;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
-
 public class ActivemqTest {
-
+/*
     @Test
     public void sendmessage() {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-activemq.xml");
@@ -23,5 +13,5 @@ public class ActivemqTest {
                 return session.createTextMessage("helloworld");
             }
         });
-    }
+    }*/
 }
