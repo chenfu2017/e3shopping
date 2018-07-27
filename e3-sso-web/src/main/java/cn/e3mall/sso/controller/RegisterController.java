116a4f0886c2c02c9814910bfe0ec98467190205
chenfu <211147056@qq.com>
2018-07-27 08:33:43 +0800

    用户注册

--- e3-sso-web/src/main/java/cn/e3mall/sso/controller/RegisterController.java
+++ e3-sso-web/src/main/java/cn/e3mall/sso/controller/RegisterController.java
@@ -1,16 +1,18 @@
 package cn.e3mall.sso.controller;
 
 import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.pojo.TbUser;
 import cn.e3mall.sso.service.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
-public class RegitsterController {
+public class RegisterController {
 
     @Autowired
     private RegisterService registerService;
@@ -23,6 +25,13 @@ public class RegitsterController {
     @ResponseBody
     public E3Result checkData(@PathVariable String param, @PathVariable Integer type) {
         E3Result e3Result = registerService.checkData(param, type);
+        return e3Result;
+    }
+
+    @RequestMapping(value="/user/register", method=RequestMethod.POST)
+    @ResponseBody
+    public E3Result register(TbUser tbUser) {
+        E3Result e3Result = registerService.register(tbUser);
         return e3Result;
     }
 }
--- e3-sso/e3-sso-service/src/main/java/cn/e3mall/sso/service/impl/RegisterServiceImpl.java
+++ e3-sso/e3-sso-service/src/main/java/cn/e3mall/sso/service/impl/RegisterServiceImpl.java
@@ -5,9 +5,12 @@ import cn.e3mall.mapper.TbUserMapper;
 import cn.e3mall.pojo.TbUser;
 import cn.e3mall.pojo.TbUserExample;
 import cn.e3mall.sso.service.RegisterService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -31,5 +34,28 @@ public class RegisterServiceImpl implements RegisterService {
             return E3Result.ok(false);
         }
         return E3Result.ok(true);
+    }
+
+    @Override
+    public E3Result register(TbUser user) {
+        if (StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())
+                || StringUtils.isBlank(user.getPhone())) {
+            return E3Result.build(400, "用户数据不完整，注册失败");
+        }
+        //1：用户名 2：手机号 3：邮箱
+        E3Result result = checkData(user.getUsername(), 1);
+        if (!(boolean) result.getData()) {
+            return E3Result.build(400, "此用户名已经被占用");
+        }
+        result = checkData(user.getPhone(), 2);
+        if (!(boolean)result.getData()) {
+            return E3Result.build(400, "手机号已经被占用");
+        }
+        user.setCreated(new Date());
+        user.setUpdated(new Date());
+        String md5Pass = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        user.setPassword(md5Pass);
+        tbUserMapper.insert(user);
+        return E3Result.ok();
     }
 }
